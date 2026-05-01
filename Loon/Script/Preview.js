8a6caa018dc2a1f8e9ab17caa6f8a7b8927e827b
莫离 <327660509@qq.com>
2026-05-01 22:59:32 +0800

    Update Preview.js

--- Loon/Script/Preview.js
+++ Loon/Script/Preview.js
@@ -1,13 +1,12 @@
 let body = $response.body || "";
-let headers = $response.headers || {};
 
 $done({
   response: {
     status: 200,
     headers: {
-      "Content-Type": "text/plain; charset=utf-8",
+      "Content-Type": "text/html; charset=utf-8",
       "Content-Disposition": "inline"
     },
-    body: body.toString()
+    body: `<pre style="white-space:pre-wrap;">${body}</pre>`
   }
 });
